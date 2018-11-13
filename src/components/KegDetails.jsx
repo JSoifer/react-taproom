7546400837c83861803cc22ba3fb3d4d31801b3d
Julianne Soifer <juliannesoifer@gmail.com>
2018-11-12 21:43:25 -0800

    style kegDetails

--- src/components/KegDetails.jsx
+++ src/components/KegDetails.jsx
@@ -6,7 +6,51 @@ import KegGraphic from './KegGraphic';
 
 function KegDetails(props){
   return (
-    <div>
+    <div className='kegDetailsContainer'>
+      <style jsx>{`
+          .kegDetailsContainer {
+            position: relative;
+            display: flex;
+            width: 90vw;
+            justify-content: space-between;
+            border: 2px solid darkgrey;
+            padding-left: 20px;
+            margin-top: 2vh;
+            background-color: #B4C297;
+            z-index: -1;
+          }
+          .buttons {
+            display: flex;
+            margin: 1vw;
+            justify-content: space-around;
+          }
+          .kegGraphicDiv {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+          }
+          .kegGraphic {
+            height: 200px;
+            width: 165px;
+            overflow: hidden;
+            position: relative;
+            display: flex;
+            justify-content: center;
+            align-items: flex-end;
+          }
+          .kegGraphic svg {
+            position: absolute;
+            z-index: 5;
+          }
+          .liquid {
+            height: 200px;
+            width: 175px;
+            background-color: yellow;
+            position: absolute;
+            transform: translateY(-10px);
+            z-index: -1;
+          }
+          `}</style>
       <div>
         <h3>Name: {props.kegName}</h3>
         <h4>Brand: {props.brand}</h4>
@@ -14,13 +58,17 @@ function KegDetails(props){
         <h4>ABV:{props.abv}</h4>
       </div>
       <div>
-        <EditButton/>
-      </div>
-      <div>
-        <SellButton/>
-      </div>
-      <div>
-        <KegGraphic/>
+        <div className='kegGraphicDiv'>
+          <div className='kegGraphic'>
+            <div className='liquid'>
+            </div>
+            <KegGraphic/>
+          </div>
+        </div>
+        <div className='buttons'>
+          <EditButton/>
+          <SellButton/>
+        </div>
       </div>
     </div>
   );
